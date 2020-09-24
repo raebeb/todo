7a6466bbf4fb32291b0f7945063b01bcd29ab39d
Francisca <raebeb@gmail.com>
2020-09-24 00:24:52 -0300

    template creado

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,6 @@
+from django.http import request
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'todo/index.html', context)
