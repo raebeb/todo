92548c5aaac55f6e5c6fe44da53642319177651d
Francisca <raebeb@gmail.com>
2020-09-24 00:52:24 -0300

    modelo creado, modficiaciones template

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    """Model definition for Todo."""
+
+    text = models.CharField(max_length=50)
+    complete = models.BooleanField(default=False)
+
+    class Meta:
+        """Meta definition for Todo."""
+
+        verbose_name = 'Todo'
+        verbose_name_plural = 'Todos'
+
+    def __str__(self):
+        return self.text
+
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,10 @@
 from django.http import request
 from django.shortcuts import render
+from .models import Todo
 
 def index(request):
-    context = {}
+    todo_list =  Todo.objects.order_by('id')
+    context = {
+        'todo_list': todo_list
+    }
     return render(request, 'todo/index.html', context)
